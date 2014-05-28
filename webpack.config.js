f572003610d8931c756689165e7e83ecb872d9e5
Larry Davis <lawdavis@adobe.com>
2014-05-28 12:39:47 -0700

    Added workaround suggested by @sokra

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 var webpack = require('webpack');
+var LibraryTemplatePlugin = require('webpack/lib/LibraryTemplatePlugin');
 
 module.exports = {
   entry: './index.js',
 
   output: {
     path: './build/',
-    filename: 'App.js',
-    library: 'App',
-    libraryTarget: 'this'
+    filename: 'App.js'
   },
 
   plugins: [
-    new webpack.optimize.DedupePlugin() // Dedupe dependencies
+    new webpack.optimize.DedupePlugin(), // Dedupe dependencies
+    function() {  // Define as a library
+      this.plugin('after-plugins', function(compiler) {
+        compiler.apply(new LibraryTemplatePlugin('App', 'this'));
+      });
+    }
   ]
 };
